6c0f5df4f6c8a9e7c203a70cb360cf91cb2e900c
phamn3 <31806033+phamn3@users.noreply.github.com>
2018-04-05 20:33:52 -0400

    update

--- WineClassification.R
+++ WineClassification.R
@@ -4,19 +4,76 @@
 wine1 <- wine1
 
 attach(wine1)
-summary(wine1)
+str(wine1) #get data types of all attributes
+
+#subsetting data
+class1 <- wine1[which(Class == 1),]
+class2 <- wine1[which(Class == 2),]
+class3 <- wine1[which(Class == 3),]
+
+summary(class1)
+summary(class2)
+summary(class3)
+
+#plotting some visualizations
+ggplot(wine1, aes(Class, Alcohol, group=Class)) + geom_boxplot() + ggtitle("Alcohol")
+ggplot(wine1, aes(Class, Malic.Acid, group=Class)) + geom_boxplot() + ggtitle("Malic Acid")
+ggplot(wine1, aes(Class, Ash, group=Class)) + geom_boxplot() + ggtitle("Ash")
+ggplot(wine1, aes(Class, Alcalinity.of.ash, group=Class)) + geom_boxplot() + ggtitle("Alcalinity of Ash")
+ggplot(wine1, aes(Class, Magnesium, group=Class)) + geom_boxplot() + ggtitle("Magnesium")
+ggplot(wine1, aes(Class, Total.phenols, group=Class)) + geom_boxplot() + ggtitle("Total Phenols")
+ggplot(wine1, aes(Class, Flavanoids, group=Class)) + geom_boxplot() + ggtitle("Flavanoids")
+ggplot(wine1, aes(Class, Nonflavanoid.phenols, group=Class)) + geom_boxplot() + ggtitle("Nonflavanoid Phenols")
+ggplot(wine1, aes(Class, Proanthocyanins, group=Class)) + geom_boxplot() + ggtitle("Proanthocyanins")
+ggplot(wine1, aes(Class, Color.intensity, group=Class)) + geom_boxplot() + ggtitle("Color Intensity")
+ggplot(wine1, aes(Class, Hue, group=Class)) + geom_boxplot() + ggtitle("Hue")
+ggplot(wine1, aes(Class, OD280.OD315.of.diluted.wines, group=Class)) + geom_boxplot() + ggtitle("OD280/OD315 of Diluted Wines")
+ggplot(wine1, aes(Class, Proline, group=Class)) + geom_boxplot() + ggtitle("Proline")
+
+#Observations
+#Generally a low margin of difference amongst attributes is observed between the 3 cultivars (i.e seen in boxplot for Ash, Alcalinity of Ash, Magnesium, etc.)
+#Are able to differentiate better on other properties (Total Phenols, Flavanoids, Color Intensity, Proline, etc.)
+#Class 1 for the Malic Acid attribute contains a lot of outliers
+#May be difficult to create distinguished clusters after running algorithms
 
 set.seed(1)
-#apply kmeans, removing first column. choosing k=3 because there are 3 cultivars
+######K-means: outliers might be a problem here
+#removing first column. choosing k=3 because there are 3 cultivars
 wclass <- kmeans(wine1[,-1], centers = 3, iter.max = 1000)
 wclass
 
 tbl <- table(wine1[,1], wclass$cluster)
-tbl
+tbl #resulting contingency table didn't produce great results
 
+#some visualizations
 #plotting kmeans results
-plot(wine1[c("Ash", "Alcalinity.of.ash")], col = wclass$cluster)
-
+ggplot(wine1, aes(Ash, Alcalinity.of.ash)) + geom_point(colour = wclass$cluster) +
+  ggtitle("Kmeans Results: Ash vs. Alcalinity of ash")
 #plotting actual cluster results
-plot(wine1[c("Ash", "Alcalinity.of.ash")], col = Class)
+ggplot(wine1, aes(Ash, Alcalinity.of.ash)) + geom_point(colour = wine1$Class) +
+  ggtitle("True Results: Ash vs. Alcalinity of ash")
+
+#cluster graph
+install.packages("factoextra")
+library(factoextra)
+fviz_cluster(wclass, data = wine1, ellipse.type = "convex", palette = "jco",
+             ggtheme = theme_minimal())
+#Cluster 1 and Cluster 2 are well defined, but Cluster 3 seems to overlap alot with 1 and 2
+
+
+
+#### PAM Algorithm aka K-medoids clustering
+install.packages("cluster")
+library(cluster)
+wpam <- pam(wine1[,-1], k=3)
+wpam
+
+tbl1 <- table(wine1[,1], wpam$clustering)
+tbl1 #same contingency table as kmeans??
+
+#PAM algorithm actually performed worse than kmeans algorithm
+#PAM has less than 50% accuracy vs. kmeans which had ~75% accuracy
+
+#visualize in cluster plot
+fviz_cluster(wpam, ggtheme = theme_minimal())
 
