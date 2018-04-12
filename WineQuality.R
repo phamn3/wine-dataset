99f8c1d5bd3822cfed737979b686db938d9bb1f1
phamn3 <31806033+phamn3@users.noreply.github.com>
2018-04-12 15:26:13 -0400

    update

--- WineQuality.R
+++ WineQuality.R
@@ -10,7 +10,7 @@ dim(ww) #~4900 rows in dataset, 12 columns
 sum(is.na(ww)) #no NA's in the dataset
 summary(ww) #physiochemical statistics; mean quality looks to be 6
 
-#visualizations to start off with
+#visualizations to start off with - looking at distributions/spread of each variable
 ggplot(ww, aes(ww$quality)) + geom_histogram(binwidth = 1) + ggtitle("Quality")
   #A lot of medium quality white wines (qualit of either 5 or 6); there are very few execellent and poor white wines
 ggplot(ww, aes(x=factor(0),y=ww$quality)) + geom_boxplot(outlier.size=1) + xlab(" ") +
@@ -44,10 +44,40 @@ ggplot(ww, aes(x=factor(0),y=sulphates)) + geom_boxplot(outlier.size=1) + xlab("
 ggplot(ww, aes(x=factor(0),y=alcohol)) + geom_boxplot(outlier.size=1) + xlab(" ") +
   ggtitle("Alcohol") + scale_x_discrete(breaks = NULL) + coord_flip()
 
-#Everything except for alcohol had outliers
-#don't think I will remove any outliers
+#Observations:
+#there are a lot of wines with a quality of either 5, 6, or 7. Very few wines are ranked as "poor" or "execellent"
+#Everything except for alcohol had alot of outliers
+#don't think I will remove any outliers because it might affect dataset
 
-#test for normality - data is not normal bc all p-values less than 0
+#Looking at how each variable varies with Quality
+ggplot(ww, aes(quality,fixed.acidity, group=quality)) + geom_boxplot(outlier.size = 1) +
+  xlab("Quality") + ylab("Fixed Acidity")
+ggplot(ww, aes(quality,volatile.acidity, group=quality)) + geom_boxplot(outlier.size = 1) +
+  xlab("Quality") + ylab("Volatile Acidity")
+ggplot(ww, aes(quality,citric.acid, group=quality)) + geom_boxplot(outlier.size = 1) +
+  xlab("Quality") + ylab("Citric Acid")
+ggplot(ww, aes(quality,residual.sugar, group=quality)) + geom_boxplot(outlier.size = 1) +
+  xlab("Quality") + ylab("Residual Sugar")
+ggplot(ww, aes(quality,chlorides, group=quality)) + geom_boxplot(outlier.size = 1) +
+  xlab("Quality") + ylab("Chlorides")
+ggplot(ww, aes(quality,free.sulfur.dioxide, group=quality)) + geom_boxplot(outlier.size = 1) +
+  xlab("Quality") + ylab("Free Sulfur Dioxide")
+ggplot(ww, aes(quality,total.sulfur.dioxide, group=quality)) + geom_boxplot(outlier.size = 1) +
+  xlab("Quality") + ylab("Total Sulfur Dioxide")
+ggplot(ww, aes(quality,density, group=quality)) + geom_boxplot(outlier.size = 1) +
+  xlab("Quality") + ylab("Density")
+ggplot(ww, aes(quality,pH, group=quality)) + geom_boxplot(outlier.size = 1) +
+  xlab("Quality") + ylab("pH")
+ggplot(ww, aes(quality,sulphates,group=quality)) + geom_boxplot(outlier.size = 1) +
+  xlab("Quality") + ylab("Sulphates")
+ggplot(ww, aes(quality,alcohol,group=quality)) + geom_boxplot(outlier.size = 1) +
+  xlab("Quality") + ylab("Alcohol")
+
+#Observation
+#clear indication that a high alcohol content equates to a high quality wine
+
+
+#test for normality - variables do not follow a normal distribution bc all p-values are less than 0
 shapiro.test(ww$fixed.acidity)
 shapiro.test(ww$volatile.acidity)
 shapiro.test(ww$citric.acid)
@@ -64,18 +94,13 @@ shapiro.test(ww$quality)
 
 install.packages("ggpubr")
 library("ggpubr")
-ggscatter(ww, x = "alcohol", y = "quality", 
-          add = "reg.line", conf.int = TRUE, 
-          cor.coef = TRUE, cor.method = "pearson",
-          xlab = "Alcohol", ylab = "Quality")
-  #Not a linear relationship
 ggscatter(ww, x = "residual.sugar", y = "density", 
           add = "reg.line", conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "pearson",
           xlab = "Residual Sugar", ylab = "Density")
 
 
-#looking at multicollinearity
+#looking at multicollinearity - calculating spearman rank correlation along with associated p values
 library("Hmisc")
 wcorr <- rcorr(as.matrix(ww),type = "spearman")
 wcorr
@@ -88,10 +113,7 @@ wcorr
 #quality correlated with alcohol = 0.44
 
 
-
-#Regression Approach: SVM, NN, regression tree, train and test
-#REC Curve = metric
-#metrics: MAD, confusion matrix
+#Potential Appraoch: classification?
 
 
 
