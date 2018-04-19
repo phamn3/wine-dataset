a0c66b03dab768948118dfec03f8f21ad1b39662
phamn3 <31806033+phamn3@users.noreply.github.com>
2018-04-19 14:58:12 -0400

    update

--- WineQuality.R
+++ WineQuality.R
@@ -1,4 +1,6 @@
 #Wine Quality Dataset - White Wine
+rm(list=ls())
+getwd()
 
 #importing dataset
 ww <- winequality.white
@@ -11,6 +13,7 @@ sum(is.na(ww)) #no NA's in the dataset
 summary(ww) #physiochemical statistics; mean quality looks to be 6
 
 #visualizations to start off with - looking at distributions/spread of each variable
+require(ggplot2)
 ggplot(ww, aes(ww$quality)) + geom_bar(stat="count") + ggtitle("Quality") +xlab("Quality")
   #A lot of medium quality white wines (qualit of either 5 or 6); there are very few execellent and poor white wines
 ggplot(ww, aes(x=factor(0),y=ww$quality)) + geom_boxplot(outlier.size=1) + xlab(" ") +
@@ -23,9 +26,9 @@ ggplot(ww, aes(x=factor(0),y=volatile.acidity)) + geom_boxplot(outlier.size=1) +
   #Alot of outliers for volatile acidity - white wine typically has lower acidity
 ggplot(ww, aes(x=factor(0),y=citric.acid)) + geom_boxplot(outlier.size=1) + xlab(" ") +
   ggtitle("Citric Acid") + scale_x_discrete(breaks = NULL) + coord_flip()
+#citric acid is also low - maybe due to white wines being more sweet and fruity
 ggplot(ww, aes(x=factor(0),y=residual.sugar)) + geom_boxplot(outlier.size=1) + xlab(" ") +
   ggtitle("Residual Sugar") + scale_x_discrete(breaks = NULL) + coord_flip()
-  #citric acid is also low - maybe due to white wines being more sweet and fruity
 ggplot(ww, aes(x=factor(0),y=chlorides)) + geom_boxplot(outlier.size=1) + xlab(" ") +
   ggtitle("Chlorides") + scale_x_discrete(breaks = NULL) + coord_flip()
   #A lot of outliers for chlorides, salt level? 
@@ -77,8 +80,7 @@ ggplot(ww, aes(quality,alcohol,group=quality)) + geom_boxplot(outlier.size = 1) 
 #clear indication that a high alcohol content equates to a high quality wine
 
 
-install.packages("ggpubr")
-library("ggpubr")
+if(!(require(ggpubr))) install.packages("ggpubr")
 ggscatter(ww, x = "residual.sugar", y = "density", 
           add = "reg.line", conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "pearson",
@@ -142,9 +144,11 @@ train.ind <- sample(1:r.ww, train.rate*r.ww, replace=FALSE)
 #construct random set of testing indices
 test.ind <- setdiff(1:r.ww,train.ind)
 #build train dataset
-train <- subset(ww[train.ind,], select = c("fixed.acidity", "volatile.acidity", "citric.acid", "residual.sugar", "chlorides","free.sulfur.dioxide", "total.sulfur.dioxide", "density", "pH", "sulphates", "alcohol")) 
+train <- subset(ww[train.ind,], select = c("fixed.acidity", "volatile.acidity", "citric.acid", "residual.sugar","chlorides",
+                                           "free.sulfur.dioxide", "total.sulfur.dioxide", "density", "pH", "sulphates", "alcohol")) 
 #build test dataset
-test <- subset(ww[test.ind,], select = c("fixed.acidity", "volatile.acidity", "citric.acid", "residual.sugar", "chlorides","free.sulfur.dioxide", "total.sulfur.dioxide", "density", "pH", "sulphates", "alcohol"))
+test <- subset(ww[test.ind,], select = c("fixed.acidity", "volatile.acidity", "citric.acid", "residual.sugar", "chlorides",
+                                         "free.sulfur.dioxide", "total.sulfur.dioxide", "density", "pH", "sulphates", "alcohol"))
 #labels for train dataset
 train.lbls <- ww$quality[train.ind]
 #labels for test dataset
@@ -152,51 +156,56 @@ test.lbls <- ww$quality[test.ind]
 
 
 #First Model - kNN
-install.packages("gmodels")
-library(gmodels)
-knn_pred <- knn(train = train, test = test, train.lbls, k=70)
+set.seed(1)
+if(!(require(gmodels))) install.packages("gmodels")
+require(class)
+knn_pred <- knn(train = train, test = test, train.lbls, k=61)
 summary(knn_pred)
 CrossTable(knn_pred,test.lbls, chisq = FALSE)
 #Accuracy
-(110+444+11)/nrow(test) #very low accuracy
-
+(116+437+12)/nrow(test) #very low accuracy
 
 #Second Model - Random Forest
 set.seed(1)
-install.packages("randomForest")
-library(randomForest)
-train.new <- cbind(train, train.lbls) #rebuilding original train dataset
+if(!(require(randomForest))) install.packages("randomForest")
+train.new <- cbind(train, train.lbls) #rebuilding original train dataset with data & labels
 names(train.new)[names(train.new) == 'train.lbls'] <- 'quality'
 rf_mod <- randomForest(quality~., data=train.new)
 rf_mod
+importance(rf_mod) #predictor importance
 rf_pred <- predict(rf_mod, newdata = test)
 table(rf_pred, test.lbls)
-(11+239+426+116+14)/nrow(test)
+(11+240+426+116+14)/nrow(test)
 
-#trying to tune RF model
+
+#trying to tune RF model using caret package
 set.seed(1)
-rf_mod_tuned <- tuneRF(train, train.lbls, stepFactor = 1.5, improve = 1e-5, ntree=500)
-print(rf_mod_tuned)
-plot(rf_mod_tuned$mtry, rf_mod_tuned$OOBError) #mtry=3 is the most accurate value
-rf_mod2 <- randomForest(quality~.,data=train.new, ntree=500, mtry=3)
+#grid search
+if(!(require(caret))) install.packages("caret")
+if(!(require(e1071))) install.packages("e1071")
+control <- trainControl(method="repeatedcv", number=10, repeats=3, search="grid")
+tunegrid <- expand.grid(.mtry=c(1:15)) #searching through mtry values of 1 through 15
+rf_gridsearch <- train(quality~., data=train.new, method="rf", tuneGrid=tunegrid, trControl=control)
+print(rf_gridsearch) #optimal mtry value = 2
+plot(rf_gridsearch)
+#rerunning RF Classifier with optimal mtry value
+rf_mod2 <- randomForest(quality~.,data=train.new, ntree=500, mtry=2)
 rf_mod2
 rf_pred2 <- predict(rf_mod2, newdata=test)
 table(rf_pred2,test.lbls)
-(10+246+428+114+14)/nrow(test)
-
+(10+244+432+114+14)/nrow(test) #accuracy
 
 #Third Model - SVM Classifier
-install.packages("e1071")
-library(e1071)
+set.seed(1)
 svm_mod <- svm(quality~., train.new)
 summary(svm_mod)
 svm_pred <- predict(svm_mod, test)
 table(svm_pred, test.lbls)
 (2+210+428+54)/nrow(test)
 
-
 #trying to tune SVM model
-svm_tune <- tune(svm, train.x=train, train.y = train.lbls, kernel="radial",  ranges=list(cost=10^(-1:2), gamma=c(.5,1,2)))
+svm_tune <- tune(svm, train.x=train, train.y = train.lbls, kernel="radial",
+                 ranges=list(cost=10^(-1:2), gamma=c(.5,1,2)))
 svm_tune #cost of 10 and gamma of 1
 svm_mod_tuned <- svm(quality~.,train.new, kernel="radial", cost=10, gamma=1)
 summary(svm_mod_tuned)
